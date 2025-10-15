8cf2eebadb9f6549aee2f8227fc210b67ae7a21c
Daniel Zegers <daniel.zegers@usm.cl>
2025-10-15 17:11:31 -0300

    Initial commit

---
+++ codegen/apis/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthApi';
+export * from './ComparisonApi';
+export * from './RequisiteApi';
+export * from './RevisionApi';
+export * from './RoleApi';
+export * from './UserApi';
---
+++ scripts/generate.ps1
@@ -0,0 +1,1 @@
+openapi-generator-cli generate -i http://localhost:8000/openapi.json -o codegen -g typescript-fetch
